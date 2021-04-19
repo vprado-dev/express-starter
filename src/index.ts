dd26bba410fb2691f4d4fbd18666e36536eaf874
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-19 12:24:42 -0300

    Add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
+import { json } from "body-parser";
+import morgan from "morgan";
 
 const app = express();
+app.use(cors());
+app.use(json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World").end();
