b227e64c8a04fc2b6df2fee174fba203d71c6950
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-19 12:18:58 -0300

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log(`Hello world`);
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World").end();
+});
+
+app.listen(3000, () => console.log("Listening at http://localhost:3000"));
