88c668ba0494974c5d124455bb1393a1b59cb22c
Vinicius Prado <vinicavagna@gmail.com>
2021-06-29 15:13:43 -0300

    :wrench: added debug message

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 (async () => {
-  app.listen(PORT, () => console.info(`Listening at http://localhost:${PORT}`));
+  app.listen(PORT, () =>
+    console.info(`🟢 Listening at http://localhost:${PORT}`),
+  );
 })().catch((err) => {
   console.log(err);
   process.exit(1);
