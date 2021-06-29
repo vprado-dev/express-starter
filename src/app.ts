ce45f5840232826bcec99dfb40b54665822b3c4c
Vinicius Prado <vinicavagna@gmail.com>
2021-06-29 15:04:13 -0300

    :sparkles: changing api default aplication entry point

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { json } from 'body-parser';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import morgan from 'morgan';
+import { notFound } from './middlewares/notFound';
+import { exception } from './middlewares/exception';
+import openapi from '@ev-fns/openapi';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(morgan('dev'));
+app.use(openapi());
+const routes = readdirSync(join(__dirname, 'routes'));
+
+for (const route of routes) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  app.use(require(join(__dirname, 'routes', route)).default);
+}
+
+app.use(exception);
+
+app.use(notFound);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,13 @@
 import dotenv from 'dotenv-safe';
+import app from './app';
 
 dotenv.config();
 
-import express from 'express';
-import cors from 'cors';
-import { json } from 'body-parser';
-import { readdirSync } from 'fs';
-import { join } from 'path';
-import morgan from 'morgan';
-import { notFound } from './middlewares/notFound';
-import { exception } from './middlewares/exception';
-import openapi from '@ev-fns/openapi';
-
-const app = express();
-
-app.use(cors());
-app.use(json());
-app.use(morgan('dev'));
-app.use(openapi());
-const routes = readdirSync(join(__dirname, 'routes'));
-
-for (const route of routes) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  app.use(require(join(__dirname, 'routes', route)).default);
-}
-
-app.use(exception);
-
-app.use(notFound);
-
-app.listen(process.env.PORT, () =>
-  console.log(`Listening at http://localhost:${process.env.PORT}`),
-);
+(async () => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Listening at http://localhost:${process.env.PORT}`),
+  );
+})().catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
