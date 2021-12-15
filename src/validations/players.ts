dee48796e77b2fde7dc1fbfff2ea6950e27b4685
Vinicius Prado <vinicavagna@gmail.com>
2021-12-15 07:25:32 -0300

    :hammer: refactored validation at route patch player

--- src/validations/players.ts
+++ src/validations/players.ts
@@ -13,9 +13,11 @@ export const playersGetOneParams = Joi.object({
 }).required();
 
 export const playersPatchOneBody = Joi.object({
-  nickName: Joi.string().required(),
-  age: Joi.number().required(),
-  level: Joi.number().required(),
-  classType: Joi.string().required(),
-  description: Joi.string().required(),
-}).required();
+  nickName: Joi.string(),
+  age: Joi.number(),
+  level: Joi.number(),
+  classType: Joi.string(),
+  description: Joi.string(),
+})
+  .min(1)
+  .required();
