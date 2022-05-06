5b69f7a52021b7c25d82d8b73a840044e10807c1
Vinicius Prado <vinicavagna@gmail.com>
2022-05-06 11:00:11 -0300

    :wrench: added some comments documentation

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,12 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    // Enviroment
     NODE_ENV: "development" | "test" | "production" | "staging";
     PORT: string;
+
+    // Api
     API_NAME: string;
     API_TOKEN: string;
+    API_IMAGE: string;
   }
 }
